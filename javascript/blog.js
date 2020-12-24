5ec5df3fb70f64c165f384a1be9f4150e4ec3a41
Orxan223 <or.xan.2018@bk.ru>
2020-12-24 19:25:45 +0400

    Update

---
+++ javascript/blog.js
@@ -0,0 +1,46 @@
+window.sr = new ScrollReveal()
+
+sr.reveal('.navbar', {
+    delay: 800
+});
+sr.reveal('.baba', {
+    delay: 800
+});
+sr.reveal('.container', {
+    distance: "60px"
+});
+sr.reveal('.btn', {
+    delay: 900,
+    opacity: 0,
+    origin: "top",
+    distance: "60px"
+
+});
+
+//--------------------------------------------------------------------------------------------------------------
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        logo.setAttribute("src", "img/indir.3..png");
+        nav.style.transition = 'all 0.5s ease-in-out';
+        nav.style.padding = ('15px 0px');
+        nav.style.background = "white";
+        nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
+        blanks.forEach(i => {
+            i.style.color = "black";
+        });
+    } else {
+        logo.setAttribute("src", "img/indir.png");
+        nav.style.transition = 'all 0.5s ease-in-out';
+        nav.style.padding = ('20px 0px');
+        nav.style.background = "none";
+        nav.style.boxShadow = "none";
+        blanks.forEach(i => {
+            i.style.color = "white";
+        })
+    }
+}
