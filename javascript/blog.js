8f70051f202862392b85962bbae00d3043c15ce2
Orxan223 <or.xan.2018@bk.ru>
2020-12-25 14:42:21 +0400

    Update

--- javascript/blog.js
+++ javascript/blog.js
@@ -17,8 +17,87 @@ sr.reveal('.btn', {
 
 });
 
-//--------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
 
+
+
+let btnAll = document.querySelector('#all');
+let btnPackaging = document.querySelector('#packaging');
+let btnMockup = document.querySelector('#mockup');
+let btnTypography = document.querySelector('#typography');
+let btnPhotography = document.querySelector('#photography');
+let items = document.querySelectorAll('.portfolio-item');
+
+btnAll.addEventListener('click', function () {
+
+    for (let i = 0; i < items.length; i++) {
+        let id = items[i].getAttribute('id')
+
+        if (id != "") {
+            items[i].parentElement.style.display = "block"
+        }
+    }
+});
+
+btnPackaging.addEventListener('click', function () {
+    for (let i = 0; i < items.length; i++) {
+        let id = items[i].getAttribute('id')
+
+        if (id != "2") {
+            items[i].parentElement.style.display = "none"
+        } else {
+            items[i].parentElement.style.display = "block"
+        }
+
+    }
+});
+
+btnMockup.addEventListener('click', function () {
+    for (let i = 0; i < items.length; i++) {
+        let id = items[i].getAttribute('id')
+        if (id != "3") {
+            items[i].parentElement.style.display = "none"
+        } else {
+            items[i].parentElement.style.display = "block"
+        }
+
+    }
+});
+
+btnTypography.addEventListener('click', function () {
+    for (let i = 0; i < items.length; i++) {
+        let id = items[i].getAttribute('id')
+        if (id != "4") {
+            items[i].parentElement.style.display = "none"
+        } else {
+            items[i].parentElement.style.display = "block"
+        }
+
+    }
+});
+
+btnPhotography.addEventListener('click', function () {
+    for (let i = 0; i < items.length; i++) {
+        let id = items[i].getAttribute('id')
+        if (id != "1") {
+            items[i].parentElement.style.display = "none"
+        } else {
+            items[i].parentElement.style.display = "block"
+        }
+
+    }
+});
+
+
+
+//-----------------------
+let nav = document.querySelector(".nava");
+let blanks = document.querySelectorAll(".blank");
+let logo = document.getElementById("darklogo");
+let header = document.getElementById("header");
+
+
+// -----------------------------------------Header ----------------------------------------
 window.onscroll = function () {
     scrollFunction();
 };
